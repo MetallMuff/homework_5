960f0b88b98ce52eb89546acffa98f286525c448
Maxim <metallmuff@gmail.com>
2022-07-10 17:05:43 +1000

    Конец второй задачи

--- Program.cs
+++ Program.cs
@@ -38,3 +38,42 @@ void PrintArray(int[] numbers)
 */
 
 //////////////Конец первой задачи /////////
+
+
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+// Console.WriteLine("Введите размер массива");
+// int size = Convert.ToInt32(Console.ReadLine());
+// int[] numbers = new int[size];
+// num_random_array(numbers);
+// Console.WriteLine("Вот наш массив: ");
+// print_array(numbers);
+// int sum = 0;
+
+// for (int i = 0; i < numbers.Length; i+=2)
+//     sum += numbers[i];
+
+//     Console.WriteLine($"всего {numbers.Length} чисел, сумма элементов на нечётных позициях = {sum}");
+
+// void num_random_array(int[] numbers)
+// {
+//     for(int i = 0; i < numbers.Length; i++)
+//         {
+//             numbers[i] = new Random().Next(1,10);
+//         }
+// }
+// void print_array(int[] numbers)
+// {
+//     Console.Write("[ ");
+//     for(int i = 0; i < numbers.Length; i++)
+//         {
+//             Console.Write(numbers[i] + " ");
+//         }
+//     Console.Write("]");
+//     Console.WriteLine();
+// }
+
+//////////////////// Конец второй задачи ///////////////
